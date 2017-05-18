a98c47bc3a974abe8701da15106bbf3d7b07005f
Alejandro Fernandez Gonzalez <Alex@alejandros-macbook-pro.local>
2017-05-18 18:03:46 +0200

    Initial Commit

---
+++ PrivateGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrivateGallery
+//
+//  Created by Alejandro Fernandez Gonzalez on 18/05/2017.
+//  Copyright © 2017 Alejandro Fernandez Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
